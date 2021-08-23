dddb05b56e3c8febf475813153beb5776c6109bd
alen6516 <alen6516@gmail.com>
2021-08-23 21:01:05 +0800

    add tmux_session.sh

---
+++ tmux_session.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# tmux send-keys -t "$pane" C-z 'pwd' Enter
+
+
+
+#SESSIONNAME="script"
+#tmux has-session -t $SESSIONNAME &> /dev/null
+#
+#if [ $? != 0 ]; then
+#    tmux new-session -s $SESSIONNAME -n script -d
+#    tmux send-keys -t $SESSIONNAME "~/bin/script" C-m 
+#fi
+#tmux attach -t $SESSIONNAME
+
+
+date=`date +%m%d`
+#time=`date +%H:%M:%S`
+time=`date +%S`
+
+file_name="$HOME/.tmux_session_$date"
+touch $file_name
+echo -n "" > $file_name
+
+for session in `tmux ls | cut -d ":" -f 1`
+do
+    #tmux send-keys -t ${session} Escape
+    #tmux send-keys -t ${session} C-z 
+    tmux new-window -t ${session} -c '#{pane_current_path}' -n ${time}
+    tmux send-keys -t ${session}:${time} "echo -n ${session}: >> ${file_name} && pwd >> ${file_name}" Enter
+    tmux kill-window -t ${session}:${time}
+
+	#echo $session >> $file_name
+	#tmux attach-session -t $session
+	#tmux select-window -l
+	#tmux new
+	#tmux send-keys C-z "echo $pwd >> $file_name" Enter
+	#tmux send-keys C-z "logout" Enter
+	#tmux detach
+done
