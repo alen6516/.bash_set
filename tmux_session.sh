dd96ee5a686af007929b468da7bc6750008dec4c
alen6516 <alen6516@gmail.com>
2021-08-29 21:35:11 +0800

    update tmux_session

--- tmux_session.sh
+++ tmux_session.sh
@@ -14,26 +14,19 @@
 #tmux attach -t $SESSIONNAME
 
 
-date=`date +%m%d-%H:%M:%S`
-wdw="tmp"
+date=`date +%m%d`
+time=`date +%H%M%S`
 
-file_name="$HOME/.tmux_session_$date"
+file_name="$HOME/.tmux_session_$date$time"
 touch $file_name
 echo -n "" > $file_name
 
 for session in `tmux ls | cut -d ":" -f 1`
 do
     #tmux send-keys -t ${session} Escape
-    #tmux send-keys -t ${session} C-z 
-    tmux new-window -t ${session} -c '#{pane_current_path}' -n ${wdw}
-    tmux send-keys -t ${session}:${wdw} "echo -n ${session}: >> ${file_name} && pwd >> ${file_name} && logout" Enter
-    #tmux kill-window -t ${session}:${wdw}
+    tmux send-keys -t ${session} C-z
+    tmux send-keys -t ${session} "echo -n ${session}: >> ${file_name} && pwd >> ${file_name}; fg" Enter
 
-	#echo $session >> $file_name
-	#tmux attach-session -t $session
-	#tmux select-window -l
-	#tmux new
-	#tmux send-keys C-z "echo $pwd >> $file_name" Enter
-	#tmux send-keys C-z "logout" Enter
-	#tmux detach
+    #tmux new-window -t ${session} -c '#{pane_current_path}' -n ${wdw}
+    #tmux send-keys -t ${session}:${wdw} "echo -n ${session}: >> ${file_name} && pwd >> ${file_name} && logout" Enter
 done
